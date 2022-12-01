dadd2c5fc620fcee3dbf2c2a94bdc51a27905739
Mateus Cruz <mateuscolvr@gmail.com>
2022-12-01 13:05:22 -0300

    Day 01 - Part 1

---
+++ python/day1.py
@@ -0,0 +1,14 @@
+with open('./day1.txt', 'r') as f:
+    data = f.read()
+
+data = [list(map(lambda x: int(x), tuple([y for y in c.split("\n") if y]))) for c in data.split("\n\n")]
+final_list = list(map(lambda x: sum(x), data))
+
+max_i = 0
+max_v = 0
+for i, v in enumerate(final_list):
+    if v > max_v and i != max_i:
+        max_i = i
+        max_v = v
+
+print((max_v, max_i + 1))
