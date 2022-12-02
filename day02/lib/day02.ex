6d22b1db26d8edbf87bb036f828693f14650a14f
Mateus Cruz <mateuscolvr@gmail.com>
2022-12-02 13:58:02 -0300

    Day 02 - Part 1

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -1,18 +1,36 @@
 defmodule Day02 do
-  @moduledoc """
-  Documentation for `Day02`.
-  """
+  @points %{"A" => 1, "B" => 2, "C" => 3, "X" => 1, "Y" => 2, "Z" => 3}
 
-  @doc """
-  Hello world.
+  def part1(input) do
+    parse_file(input) |> Enum.map(fn x -> match(x) end) |> Enum.reduce(fn acc, v -> acc + v end)
+  end
 
-  ## Examples
+  defp parse_file(f) do
+    file = File.read(f)
 
-      iex> Day02.hello()
-      :world
+    case file do
+      {:ok, content} ->
+        content
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn x -> x |> String.split(" ") end)
 
-  """
-  def hello do
-    :world
+      {:error, _} ->
+        []
+    end
+  end
+
+  defp match([enemy, you]) do
+    p1 = @points[enemy]
+    p2 = @points[you]
+
+    cond do
+      p1 == p2 -> 3 + p2
+      enemy == "A" and you == "Y" -> 6 + p2
+      enemy == "A" and you == "Z" -> 0 + p2
+      enemy == "B" and you == "X" -> 0 + p2
+      enemy == "B" and you == "Z" -> 6 + p2
+      enemy == "C" and you == "X" -> 6 + p2
+      enemy == "C" and you == "Y" -> 0 + p2
+    end
   end
 end
