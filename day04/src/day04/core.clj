ce8501a0b3d8d8c44d98dfa452a57285c4c32048
Mat <mateuscolvr@gmail.com>
2022-12-04 09:39:31 -0300

    Day 04 - Part 1

---
+++ day04/src/day04/core.clj
@@ -0,0 +1,36 @@
+(ns day04.core)
+(require '[clojure.string :as str])
+
+(defn to-range
+  [[x y]]
+  (let [parse-int #(Integer/parseInt %)]
+  (range (parse-int x) (+ (parse-int y) 1))))
+
+(defn subseq?
+  [x y]
+  (let [size-x (count x)
+        size-y (count y)]
+  (if (< size-x size-y)
+  (some #{x} (partition (count x) 1 y))
+  (some #{y} (partition (count y) 1 x)))))
+
+(defn parse-data
+  [input]
+  (let [raw-data (map #(str/split % #",") (str/split input #"\n"))
+        split-at-dash #(str/split % #"-")
+        fonfon #(to-range (split-at-dash %))]
+        (map #(map fonfon %) raw-data)))
+
+(defn pre-process
+  [data]
+  (map #(let [[x y] %] (subseq? x y)) data))
+
+(defn part-one
+  [input]
+  (let [parsed-data (parse-data input)
+        pre-processed (pre-process parsed-data)]
+  (reduce #(+ % %2) (map #(if % 1 0) pre-processed))))
+
+(defn -main
+  []
+  (println (str "Part one: " (part-one (slurp "input.txt")))))
